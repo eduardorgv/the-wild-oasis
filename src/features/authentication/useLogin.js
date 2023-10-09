95e873f014448de7e94339038eeb68493fe69d0d
eduardorgv <eduardsabb@outlook.com>
2023-10-09 13:36:38 -0600

    Register new user

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -2,7 +2,6 @@ import { login as loginApi } from "../../services/apiAuth";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import { redirect } from "react-router-dom";
 
 export function useLogin() {
   const navigate = useNavigate();
