0cd03e2a1887bfafd4dc3d0fe4445229756f48be
eduardorgv <eduardsabb@outlook.com>
2023-10-09 14:59:26 -0600

    Update user

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,6 +1,6 @@
-import ButtonIcon from "../../ui/ButtonIcon";
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 import { useLogout } from "./useLogout";
+import ButtonIcon from "../../ui/ButtonIcon";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 export const Logout = () => {
---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,28 @@
+import { HiOutlineUser } from "react-icons/hi2";
+import { Logout } from "../features/authentication/Logout";
+import { useNavigate } from "react-router-dom";
+import ButtonIcon from "./ButtonIcon";
+import styled from "styled-components";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+export const HeaderMenu = () => {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+};
