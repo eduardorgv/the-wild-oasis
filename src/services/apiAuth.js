9e445e2f7c3e5c5b3fe371305eeb80061b1ff8eb
eduardorgv <eduardsabb@outlook.com>
2023-10-09 16:35:28 -0600

    Fixed: update user

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -58,7 +58,7 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
 
   // 3. Update avatar in the user
   const { data: updatedUser, error: updatedUserError } =
-    supabase.auth.updateUser({
+    await supabase.auth.updateUser({
       data: {
         avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
       },
